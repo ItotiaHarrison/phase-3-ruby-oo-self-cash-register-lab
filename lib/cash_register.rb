a6ac8943086652e605dbac30a5882389fe74faab
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-22 18:17:07 +0300

    commit tree 8ac44fe2543472b083533dfb725b6e037cfa8eec

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,12 +11,12 @@ class CashRegister
     def add_item(title, price, quantity = 1)
         @items << {title: title, price: price, quantity: quantity}
         
-        @total += price*quantity
+        self.total += price*quantity
     end
 
     def apply_discount
         if(@discount > 0)
-            @total = @total*(100 - @discount)/100
+            self.total = self.total*(100 - self.discount)/100
             "After the discount, the total comes to $#{@total.to_i}."
         else
             "There is no discount to apply."
@@ -35,7 +35,7 @@ class CashRegister
 
     def void_last_transaction
         @items.delete(-1)
-        @total -= (@items[-1][:price]).to_f * (@items[-1][:quantity]).to_f
+        self.total -= (@items[-1][:price]).to_f * (@items[-1][:quantity]).to_f
         @total
     end
 end
